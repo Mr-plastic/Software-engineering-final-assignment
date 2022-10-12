c56a03308ba88e11807c436f6ebf855430375aa4
Mrplastic <ppic5413@gmail.com>
2022-10-12 11:37:11 +0800

    添加背景圖片與登入功能修改

--- Best-Flutter-UI-Templates/best_flutter_ui_templates/lib/custom_drawer/Login_In.dart
+++ Best-Flutter-UI-Templates/best_flutter_ui_templates/lib/custom_drawer/Login_In.dart
@@ -3,6 +3,11 @@ import 'package:best_flutter_ui_templates/navigation_home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+String regexEmail = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*\$";
+String regexPassword = r'^(?=.*?[a-z])(?=.*?[0-9]).{6,30}$';
+bool check_email = false;
+bool check_password = false;
+
 class LoginIN extends StatefulWidget {
   const LoginIN({Key? key}) : super(key: key);
 
@@ -30,31 +35,77 @@ class _LoginIN extends State<LoginIN> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'email',
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/backgroundpicture.png"),
+                fit: BoxFit.cover)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'email',
+                filled: true,
+                fillColor: Colors.white,
+              ),
+              validator: (value) {
+                print('pass_1');
+                if (value!.isEmpty) {
+                  return '不能為空';
+                }
+                check_email = RegExp(regexEmail).hasMatch(value);
+                if (!check_email) {
+                  return '信箱不符合格式';
+                }
+                return null;
+              },
             ),
-          ),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Password',
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              print('Login pressed ...');
-              Navigator.push(context, MaterialPageRoute(builder: (context) => NavigationHomeScreen()));
-            },
-            child: Text('Login'),
-          ),
-        ],
+            TextFormField(
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+                filled: true,
+                fillColor: Colors.white,
+              ),
+              validator: (value) {
+                print('pass_2');
+                if (value!.isEmpty) {
+                  return '不能為空';
+                }
+                check_password = RegExp(regexPassword).hasMatch(value);
+                if (!check_password) {
+                  return '密碼不符合格式';
+                }
+                print('pass_2');
+                return null;
+              },
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (check_email & check_password) {
+                  print('Login pressed ...');
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => NavigationHomeScreen()));
+                }
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
       ),
     );
   }
