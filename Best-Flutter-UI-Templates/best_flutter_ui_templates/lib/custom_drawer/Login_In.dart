378c6c1d72c2c56b283859401684268da5042618
orange2000121 <danny1.2104@gmail.com>
2022-10-16 12:03:55 +0800

    form

--- Best-Flutter-UI-Templates/best_flutter_ui_templates/lib/custom_drawer/Login_In.dart
+++ Best-Flutter-UI-Templates/best_flutter_ui_templates/lib/custom_drawer/Login_In.dart
@@ -3,7 +3,7 @@ import 'package:best_flutter_ui_templates/navigation_home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-String regexEmail = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*\$";
+String regexEmail = r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
 String regexPassword = r'^(?=.*?[a-z])(?=.*?[0-9]).{6,30}$';
 bool check_email = false;
 bool check_password = false;
@@ -16,19 +16,17 @@ class LoginIN extends StatefulWidget {
 }
 
 class _LoginIN extends State<LoginIN> {
-  TextEditingController? textController1;
-
-  TextEditingController? textController2;
+  TextEditingController? textController1 = TextEditingController();
+  TextEditingController? textController2 = TextEditingController();
 
   late bool passwordVisibility;
 
-  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final emailFormKey = GlobalKey<FormState>();
+  final passwordFormKey = GlobalKey<FormState>();
 
   @override
   void initState() {
     super.initState();
-    textController1 = TextEditingController();
-    textController2 = TextEditingController();
     passwordVisibility = false;
   }
 
@@ -38,73 +36,75 @@ class _LoginIN extends State<LoginIN> {
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/backgroundpicture.png"),
-                fit: BoxFit.cover)),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'email',
-                filled: true,
-                fillColor: Colors.white,
+        decoration: BoxDecoration(image: DecorationImage(image: AssetImage("assets/images/backgroundpicture.png"), fit: BoxFit.cover)),
+        child: Form(
+          key: emailFormKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                obscureText: false,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'email',
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                validator: (value) {
+                  print('pass_1');
+                  if (value!.isEmpty) {
+                    return '不能為空';
+                  }
+                  check_email = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value);
+                  if (!check_email) {
+                    return '信箱不符合格式';
+                  }
+                  return null;
+                },
               ),
-              validator: (value) {
-                print('pass_1');
-                if (value!.isEmpty) {
-                  return '不能為空';
-                }
-                check_email = RegExp(regexEmail).hasMatch(value);
-                if (!check_email) {
-                  return '信箱不符合格式';
-                }
-                return null;
-              },
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            TextFormField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-                filled: true,
-                fillColor: Colors.white,
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
               ),
-              validator: (value) {
-                print('pass_2');
-                if (value!.isEmpty) {
-                  return '不能為空';
-                }
-                check_password = RegExp(regexPassword).hasMatch(value);
-                if (!check_password) {
-                  return '密碼不符合格式';
-                }
-                print('pass_2');
-                return null;
-              },
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (check_email & check_password) {
-                  print('Login pressed ...');
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => NavigationHomeScreen()));
-                }
-              },
-              child: Text('Login'),
-            ),
-          ],
+              TextFormField(
+                obscureText: false,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                validator: (value) {
+                  print('pass_2');
+                  if (value!.isEmpty) {
+                    return '不能為空';
+                  }
+                  check_password = RegExp(regexPassword).hasMatch(value);
+                  if (!check_password) {
+                    return '密碼不符合格式';
+                  }
+                  print('pass_2');
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (emailFormKey.currentState!.validate()) {
+                    if (check_email & check_password) {
+                      print('Login pressed ...');
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => NavigationHomeScreen()));
+                    } else {
+                      print('email check: $check_email');
+                      print('password check: $check_password');
+                    }
+                  }
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
         ),
       ),
     );
